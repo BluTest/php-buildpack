f9f69ea6aba5ac869576b801f3a4f63916d01e25
TestManual (Test) <testmanual@blubracket.com>
2021-12-28 16:17:59 -0800

    Create somethings.sh

---
+++ scripts/somethings.sh
@@ -0,0 +1,112 @@
+1. Gender
+
+It’s still a man’s world (unfortunately). And our language is full of non-inclusive gendering.
+
+Take the use of “man” — we still use the term man in job titles (Anchorman, Chairman, Congressman, Doorman, Fireman, Garbage Man, Handyman, etc.) instead of gender-neutral job titles.
+
+And we also use “man” in words for which there is no “woman” alternative (e.g. “mankind”, “manpower”, “man-made”, “workmanship”, “layman” etc.
+
+And how about the pronoun “he”? — Most versions of the bible speak of the Holy Spirit in masculine terms (“he”). Many of us tell stories about animals and more often use the pronoun “he” versus “she” to describe the main characters.
+
+And “father” still seems to know best. We still use “grandfathered in” or “grandfather clause,” — but no one uses “grandmothered in” or “grandmother clause.”
+
+ 
+2. Race
+
+Color — “The colors black and white have long carried opposite connotations. Black has connoted evil and disgrace, while white has connoted decency and purity…” — Color Connotations and Racial Attitudes by Douglas Longshore from UCLA.
+
+If you are “blacklisted,” you are rejected, whereas “whitelisted” means you’re approved. A person can be “blackballed”(rejected), but there is no “white-balled.”
+
+In technology, “black hat” status means that you’ve done something bad whereas “white hat” status means you’re ok.
+
+“no can do”? — Did you know this phrase was historically used as a way to mock Chinese people in the U.S.?
+
+And the “brown bag sessions” you host to bring people together might offend some. The Brown Paper Bag Test (aka “Brown Bag” test) was a form of racial discrimination in the African-American community in the 20th century. The test compared an individual’s skin tone to the color of a brown paper bag. Darker skin tones were denied access to events, clubs, and businesses.
+
+ 
+3. Immigrants
+
+Not everyone born in a country stays there to live and work.
+
+And not everyone speaks English as a 1st language — “native English speaker” is another example of non-inclusive language.
+
+“Illegal alien” is offensive to people who are immigrants in any country. “Illegal alien” dehumanizes the migrant community.
+
+Did you know that “anchor baby” is non inclusive language? President Trump used this to describe children with non-citizen parents and got major blowback for using non-inclusive language (and being offensive).
+
+ 
+4. LGBTQ
+
+People from the LGBT+ community are often left out based on personal pronouns, how they describe their families, or who they choose to love.
+
+“He” and “She” are non inclusive and assume gender identification. Someone who is gender non-binary might not use either pronoun. Personal pronouns are important to the LBTQ+ community.
+
+And so is how you describe a family. “Mom and dad” or “husband and wife” doesn’t always fit.
+
+How about “sexual preference”? Sexual preference suggests a degree of choice — sexual orientation is not a choice.
+
+And calling someone “homo” is offensive to people in the LGBTQ+ community. “Homosexual” was once used to describe the gay community but is not anymore.
+
+ 
+5. Age
+
+Are you a Baby Boomer, Gen X’r, Gen Y’r, Gen Z’r, or Gen A’r? These generations span over 75 years — and there’s language that applies to each of them differently.
+
+Are Baby Boomers (gasp) “old geezers”? This is non-inclusive language because using the term “geezer” in the U.S. implies frailty.
+
+How about a “digital native.” “Digital native” describes a person born or brought up during the age of digital technology. Using it is non-inclusive to older people AND increases the risk of age discrimination lawsuits in business.
+
+And “millennials only” or “recent graduates only” are on the non inclusive list for the same reason above.
+
+ 
+6. Physical Disability
+
+The Americans with Disabilities Act (ADA) was signed into law on July 26, 1990, by President George H.W. Bush. The ADA prohibits being non-inclusive and guarantees people with disabilities have the same opportunities as everyone else (employment opportunities included).
+
+Have you been to an event where the speaker asks you to “stand” or “please be seated”? People with a physical disability might not be able to do one or the other.
+
+And there’s “type” — many jobs require candidates to “type” X words per minute, but some may not physically be able to “type” on a keyboard.
+
+“Walk” — seems like a common thing for people to do, right? But a physical disability might keep some from “walking.”
+
+ 
+7. Mental Health
+
+I’m sure at some point in your life, you’ve used “crazy” or “nuts” to describe a situation. I know I have…But, could we be offending someone with a mental illness?
+
+Another one of the non-inclusive language examples relating to mental health is “retarded” — it’s casually used all too often, even if no harm is meant by it.
+
+And what about “psycho”? It might be great if you are into horror movies. But it’s on the non inclusive list because it might offend someone dealing with mental health challenges.
+
+Do you say let’s do a “sanity check” when double-checking something? You might be unknowingly using non-inclusive language if so. Using the word “sanity” might offend someone with a neurodiversity or mental illness.
+
+ 
+8. Elitism
+
+Elitism is still alive and well in many clubs, business circles, and beyond. Elitist language also makes the non inclusive list.
+
+Only allowing access or employing someone because they have a “degree from a top 10 school” or “a degree from an Ivy League University” is non inclusive.
+
+And how about people who don’t test well? Not everyone gets an “SAT score of 1200+”.
+
+ 
+9. Former Felons
+
+If you’ve ever been in trouble with the law…you know it can affect background checks (or how people perceive you as a person). But people make mistakes — and deserve second chances for inclusion in society.
+
+Calling someone a “felon” suggests that if they have a criminal record — they are still a criminal now.
+
+And some states forbid the use of non inclusive language examples like “have no felony background” or “no convicted felons” when advertising jobs.
+
+ 
+10. Religion
+
+Politics and religion are not topics people always like to discuss. There are loads of examples of non inclusive language examples based on religion, and some are too bad to type here. But, here are a few.
+
+“Bible beater” is a negative term people use to describe religious people who follow Christianity.
+
+And, “Heathen” is used to describe someone who strays from a religion.
+
+Both are non-inclusive and might offend people who have certain religious beliefs.
+
+And, how about assuming people are taking a vacation for the “Christmas holiday”? This is non inclusive to other religions like Judaism, Islam, and others.
